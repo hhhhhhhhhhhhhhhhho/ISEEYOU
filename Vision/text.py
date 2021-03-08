b78acf9f3a2e8d2ed57ae74f0727f318df31dc98
cksrb63 <73516336+ckrb63@users.noreply.github.com>
2021-03-08 23:43:04 +0900

    실시간 웹캠 글자인식

--- Vision/text.py
+++ Vision/text.py
@@ -1,8 +1,22 @@
 import pytesseract
 import cv2
+import numpy as np
 
 pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract'
-print(pytesseract.image_to_string('idcard.jpg', lang='kor'))
+video_capture = cv2.VideoCapture(0)
 
+
+
+while True:
+    ret, frame = video_capture.read()
+    print(pytesseract.image_to_string(frame, lang='kor'))
+    #name=pytesseract.image_to_string(image, lang='kor')
+    cv2.imshow('Video', frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cv2.waitKey(0)
+video_capture.release()
+cv2.destroyAllWindows()
 
 
