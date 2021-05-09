bc0c0dd3ee8a2528361a9bd2e07ce55181752e32
mokseungju <mokseungju@naver.com>
2021-05-10 00:41:18 +0900

    modify mainApp.py and testpage.py

--- Application/mainApp.py
+++ Application/mainApp.py
@@ -4,6 +4,5 @@ import sys
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    #ex = Application.widgets.Login()
     ex = Application.widgets.MainWidget()
     sys.exit(app.exec_())
