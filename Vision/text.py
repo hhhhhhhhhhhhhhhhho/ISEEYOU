32cf326902fd6673b7b95811ca72ad737a7c4885
Hyo-Kyeong <nbgreen@naver.com>
2021-06-06 11:03:42 +0900

    신분증 인식 시간초과

--- Vision/text.py
+++ Vision/text.py
@@ -2,7 +2,7 @@ import pytesseract
 import cv2
 import numpy as np
 from Database import DBconnection as DB
-
+from Application import widgets
 pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract'
 
 
@@ -25,10 +25,11 @@ def general(frame):
     diff_img = 255 - cv2.absdiff(frame[:, :, 1], bg_img)
     norm_img = cv2.normalize(diff_img, None, alpha=0, beta=255, norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_8UC1)
     th = cv2.adaptiveThreshold(norm_img, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 27, 10)
-    cv2.imshow('th', th)
     return textRec(th)
 
 def idcheck(exam_id, student_id, student_name):
+    MAX_SCD = 100
+    cur_scd = 0
     video_capture = cv2.VideoCapture(0)
     check_success = False
     while check_success==False:
@@ -38,8 +39,15 @@ def idcheck(exam_id, student_id, student_name):
         ret, frame = video_capture.read()
         stname=general(cropImage(x, x_e, y, y_e, frame))
         cv2.imshow('Video', frame)
+        cur_scd += 1
         if(stname.find(student_name)>0):
             check_success = True
+            break
+        if cur_scd >= MAX_SCD:
+            video_capture.release()
+            cv2.destroyAllWindows()
+            widgets.TimeLimit()
+            check_success = False
             break
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
